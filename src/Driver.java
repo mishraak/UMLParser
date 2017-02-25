a053a12f38aed3648f74a96d1455c1806c92c594
mishraak <mishra.akshay91@gmail.com>
2017-02-24 21:02:35 -0800

    creating compilation units out of each file

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+
+import com.github.javaparser.ParseException;
+
 public class Driver {
-	public static void main(String[] args) {
-		Parser parser = new Parser("/Users/akshaymishra/Desktop/input/", "outDiagram");
+	public static void main(String[] args) throws ParseException, IOException {
+		Parser parser = new Parser("/Users/akshaymishra/Desktop/input", "outDiagram");
 		parser.triggerParse();
 	}
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,33 +1,68 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 
 public class Parser {
 	final String inDir;
 	final String outDir;
 	ArrayList<CompilationUnit> cunits;
+	HashMap<String, String> mapClassOrInterface;
 	
     Parser(String in, String out) {
         this.inDir = in;
         this.outDir = this.outDir + "/" + out + ".png";
     }
 
-	public void triggerParse() {
+	public void triggerParse() throws ParseException, IOException {
 		//get list of all compilation units in an array list
 		cunits = getCunits(inDir);
+/*		for ( CompilationUnit cunit : cunits) {
+			System.out.println(cunit);
+		}*/
+		//buildMapOfCI(cunits);
+		
 	}
 	
+	private void buildMapOfCI(ArrayList<CompilationUnit> cunits) {
+        for (CompilationUnit cu : cunits) {
+            List<TypeDeclaration> typesList = cu.getTypes();
+            for (Node node : typesList) {
+                ClassOrInterfaceDeclaration cOrI = (ClassOrInterfaceDeclaration) node;
+                mapClassOrInterface.put(cOrI.getName(), cOrI.isInterface() ? "INTERFACE" : "CLASS" ); 
+            }
+        }
+	}
+	
+	
 	//http://stackoverflow.com/questions/32178349/parse-attributes-from-java-files-using-java-parser
-	private ArrayList<CompilationUnit> getCunits(String inDir) {
+	private ArrayList<CompilationUnit> getCunits(String inDir) throws ParseException, IOException {
 		ArrayList<CompilationUnit> cunitsArray = new ArrayList<CompilationUnit>();
 		File folder = new File(inDir);
 		File[] listOfFiles = folder.listFiles();
 		for (int i = 0; i < listOfFiles.length; i++) {
-	      if (listOfFiles[i].isFile()) {
-	        System.out.println("File " + listOfFiles[i].getName());
-	      } else if (listOfFiles[i].isDirectory()) {
-	        System.out.println("Directory " + listOfFiles[i].getName());
-	      }
+			if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".java")) {
+				//System.out.println(listOfFiles[i].getName());
+				FileInputStream fis = new FileInputStream(listOfFiles[i]);
+				//System.out.println(fis instanceof FileInputStream);
+				CompilationUnit cunit;
+				//System.out.println(cunit instanceof CompilationUnit);
+                try {
+                    cunit = JavaParser.parse(fis);
+                    cunitsArray.add(cunit);
+                } finally {
+                	fis.close();
+                }
+			}		
 		}
 		return cunitsArray;
 	}
