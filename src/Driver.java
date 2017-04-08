01c27aadf28aedb67e91addcda15a60cc5b37653
mishraak <mishra.akshay91@gmail.com>
2017-04-08 16:13:43 -0700

    updating umlparser1

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,12 @@ import com.github.javaparser.ParseException;
 
 public class Driver {
 	public static void main(String[] args) throws ParseException, IOException {
-		PreParser parser = new PreParser("/Users/akshaymishra/Desktop/input", "outDiagram");
-		parser.triggerParse();
+		Parser parser = new Parser("/Users/akshaymishra/Desktop/input", "outDiagram");
+		try {
+			parser.triggerParse();
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.BodyDeclaration;
@@ -11,7 +18,16 @@ import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.TypeDeclaration;
 
 public class Parser {
+	String inDir;
+	String outDir;
+	ArrayList<CompilationUnit> cunits;
+	static Map<String, Boolean> mapClassOrInterface = new HashMap<String, Boolean>();
+	static String umlString ;
 	
+	public Parser(String in, String out){
+		inDir=in;
+		outDir=out;
+	}
 	
 	public static String parseCode(CompilationUnit cu) {
 
@@ -45,29 +61,30 @@ public class Parser {
         for (BodyDeclaration bodyDecl : ((TypeDeclaration) node).getMembers()) {
           if (bodyDecl instanceof ConstructorDeclaration) {
         	ConstructorDeclaration construcDecl = (ConstructorDeclaration) bodyDecl;
-        	boolean isPublic = construcDecl.getDeclarationAsString().startsWith("public"), isInterface = corid.isInterface();
+        	boolean isPublic = construcDecl.getDeclarationAsString().startsWith("public"), 
+        			isInterface = corid.isInterface();
         	
         	if ( isPublic && !isInterface) {
         		if (isThereAnotherParam){   
         			functions = functions + ";";
-    			functions = functions + "+ ";
-    			functions = functions + construcDecl.getName();
-    			functions = functions + "(";
+	    			functions = functions + "+ ";
+	    			functions = functions + construcDecl.getName();
+	    			functions = functions + "(";
         			
                 for (Object childrenNode : construcDecl.getChildrenNodes()) {
                     if (childrenNode instanceof Parameter) {
                          param = (Parameter) childrenNode;
                          paramCls = param.getType().toString();
                          paramNm = param.getChildrenNodes().get(0).toString();
-                         
+                         //System.out.println(paramNm);
                         functions = functions + paramNm + " : " + paramCls;
-                        if (PreParser.getMapClassOrInterface().containsKey(paramCls) 			//check for the existence in keys
-                        		&& !(PreParser.getMapClassOrInterface().get(classNmMap))) {		//check using separately stored class name, shouldn't repeat
+                        if (mapClassOrInterface.containsKey(paramCls) 			//check for the existence in keys
+                        		&& !(mapClassOrInterface.get(classNmMap))) {		//check using separately stored class name, shouldn't repeat
                         	extras = extras + "["; 
                         	extras = extras + classNmMap;
                         	extras = extras + "] uses -.->";
                         	
-                            if (PreParser.getMapClassOrInterface().get(paramCls)) {
+                            if (mapClassOrInterface.get(paramCls)) {
                             	extras = extras + "[<<interface>>;"; 
                             	extras = extras + paramCls;
                             	extras = extras+ "]";
@@ -83,13 +100,13 @@ public class Parser {
                 }
                    
 	            functions = functions + ")";
-	            System.out.println(functions);
+	            //System.out.println(functions);
 	            isThereAnotherParam = true;
         		}
         	}
           }
         }
-        
+
         for (BodyDeclaration bodyDecl : ((TypeDeclaration) node).getMembers()) {
         	 if (bodyDecl instanceof MethodDeclaration) {
         		 MethodDeclaration methodDecl = (MethodDeclaration) bodyDecl;
@@ -100,31 +117,28 @@ public class Parser {
         			 boolean isSetter=methodDecl.getName().startsWith("set");
         			 if ( isGetter || isSetter) {
         				 publicFields.add(methodDecl.getName().substring(3)); //3 for the attribute
-        				 //System.out.println(methodDecl.getName().substring(3));
+        				 System.out.println(methodDecl.getName().substring(3));
         			 }
         			 else {
         				 if (isThereAnotherParam){   
-        	        			functions = functions + ";";
-        	        	 
+        	        			functions = functions + ";"; 
         	        			functions = functions + "+ "; 
         	        			functions = functions + methodDecl.getName();
         	        			functions = functions + "(";
         	        			
-        	        			
         	        			for (Object childrenNode : methodDecl.getChildrenNodes()) {
                                     if (childrenNode instanceof Parameter) {
                                         Parameter paramCast = (Parameter) childrenNode;
-                                        String paramClass = paramCast.getType()
-                                                .toString();
-                                        String paramName = paramCast.getChildrenNodes()
-                                                .get(0).toString();
+                                        String paramClass = paramCast.getType().toString();
+                                        String paramName = paramCast.getChildrenNodes().get(0).toString();
+                                        
                                         functions = functions +  paramName + " : " + paramClass;
-                                        if (PreParser.getMapClassOrInterface().containsKey(paramClass)	//check for existence in map
-                                                && !PreParser.getMapClassOrInterface().get(classNmMap)) {	//check if it is interface
+                                        if (mapClassOrInterface.containsKey(paramClass)	//check for existence in map
+                                                && !mapClassOrInterface.get(classNmMap)) {	//check if it is interface
                                         	extras = extras + "["; 
                                         	extras = extras + classNmMap;
                                         	extras = extras + "] uses -.->";
-                                            if (PreParser.getMapClassOrInterface().get(paramClass)){
+                                            if (mapClassOrInterface.get(paramClass)){
                                             	extras = extras + "[<<interface>>;";
                                             	extras = extras + paramClass; 
                                             	extras = extras + "]";
@@ -138,30 +152,80 @@ public class Parser {
                                         extras += ",";
                                     } else {
                                         String methodBody[] = childrenNode.toString().split(" ");
-                                        for (String foo : methodBody) {
-                                            if (PreParser.getMapClassOrInterface().containsKey(foo)
-                                                    && !PreParser.getMapClassOrInterface().get(classNmMap)) {
+                                        for (String mb : methodBody) {
+                                            if (mapClassOrInterface.containsKey(mb)
+                                                    && !mapClassOrInterface.get(classNmMap)) {
                                             	extras += "[" + classNmMap
                                                         + "] uses -.->";
-                                                if (PreParser.getMapClassOrInterface().get(foo))
-                                                	extras += "[<<interface>>;" + foo
+                                                if (mapClassOrInterface.get(mb))
+                                                	extras += "[<<interface>>;" + mb
                                                             + "]";
                                                 else
-                                                	extras += "[" + foo + "]";
+                                                	extras += "[" + mb + "]";
                                                 extras += ",";
                                             }
                                         }
                                     }
-                            }
-        			 }
+        	        			}
+        				   }
+        			  }
         		 }
-        		 
         	 }
-        }	
+        	 
+        	 //System.out.println(extras);
+		}   
+        return fresult;
+	}
+	public void triggerParse() throws ParseException, IOException {
+		String astString = "";
+		// get list of all compilation units in an array list
+		cunits = getCunits(inDir);
+		/*
+		 * for ( CompilationUnit cunit : cunits) { System.out.println(cunit); }
+		*/
+		
+		// Class or Interface
+		for (CompilationUnit cunit : cunits) {
+			 //System.out.println(cunit);
+			List<TypeDeclaration> typesList = cunit.getTypes();
+			for (Node node : typesList) {
+				// System.out.println(node);
+				ClassOrInterfaceDeclaration cOrI = (ClassOrInterfaceDeclaration) node;
+				// System.out.println(cOrI.isInterface());
+				mapClassOrInterface.put(cOrI.getName(), cOrI.isInterface());
+			}
 		}
-        return umlString.toString();
-	}		
-			
 
+		// Each compilation unit undergoes parsing now
+		for (CompilationUnit cu : cunits)
+			umlString += Parser.parseCode(cu);
+
+		 //System.out.println(umlString);
+	}
+			
+	// http://stackoverflow.com/questions/32178349/parse-attributes-from-java-files-using-java-parser
+	private ArrayList<CompilationUnit> getCunits(String inDir) throws ParseException, IOException {
+		ArrayList<CompilationUnit> cunitsArray = new ArrayList<CompilationUnit>();
+		File folder = new File(inDir);
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".java")) {
+				// System.out.println(listOfFiles[i].getName());
+				FileInputStream filestream = new FileInputStream(listOfFiles[i]);
+				// System.out.println(fis instanceof FileInputStream);
+				// System.out.println(cunit instanceof CompilationUnit);
+				try {
+					cunitsArray.add(JavaParser.parse(filestream));
+				} catch (com.github.javaparser.ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				finally {
+					filestream.close();
+				}
+			}
+		}
+		return cunitsArray;
+	}
 	
 }
